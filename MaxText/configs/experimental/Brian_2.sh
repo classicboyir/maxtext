ac09b3e2a896497ec619a1799e1d79579acc9506
Hossein Sarshar <hosseins@google.com>
2024-03-05 12:41:23 -0500

    a

--- MaxText/configs/experimental/Brian_2.sh
+++ MaxText/configs/experimental/Brian_2.sh
@@ -3,4 +3,4 @@ python3 MaxText/train.py MaxText/configs/base.yml run_name=hosseins-v5e-16 \
         ici_fsdp_parallelism=4 ici_tensor_parallelism=4 steps=5 remat_policy=full per_device_batch_size=1 \
         reuse_example_batch=1 enable_profiler=true \
         base_output_directory=gs://maxtext-logs-dogfood-proj/maxtext-logs/ \
-        dataset_path=gs://northam-ce-mlai-tpu/vlp-2nic-maxtext-data/
+        dataset_path=gs://maxtext-logs-dogfood-proj/dataset
