0208e8cd48c75c1690752276fd7fa9f3f2338ba7
Hossein Sarshar <hosseins@google.com>
2024-03-05 09:30:05 -0500

    a

--- MaxText/configs/experimental/Brian_2.sh
+++ MaxText/configs/experimental/Brian_2.sh
@@ -1,5 +1,5 @@
 export LIBTPU_INIT_ARGS='--xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE'; 
-python3 MaxText/train.py MaxText/configs/base.yml run_name=hosseins-ss-32-baseconfig-profile-run1 \
+python3 MaxText/train.py MaxText/configs/base.yml run_name=hosseins-maxtext-v4-32 \
         ici_fsdp_parallelism=4 ici_tensor_parallelism=2 steps=5 remat_policy=full per_device_batch_size=1 \
         reuse_example_batch=1 enable_profiler=true \
         base_output_directory=gs://maxtext-logs-northam-test/maxtext-logs/ \
