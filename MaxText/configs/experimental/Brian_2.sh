fbedb192af329e5c462fde1b9e5f0bb5b93dde78
Hossein Sarshar <hosseins@google.com>
2024-03-06 12:28:08 -0500

    a
    exit()
    
    asdasd

--- MaxText/configs/experimental/Brian_2.sh
+++ MaxText/configs/experimental/Brian_2.sh
@@ -1,6 +1,6 @@
 export LIBTPU_INIT_ARGS='--xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE'; 
 python3 MaxText/train.py MaxText/configs/base.yml run_name=hosseins-v5e-16 \
-        ici_fsdp_parallelism=4 ici_tensor_parallelism=4 remat_policy=full per_device_batch_size=16 \
+        ici_fsdp_parallelism=4 ici_tensor_parallelism=4 remat_policy=full per_device_batch_size=14 \
         reuse_example_batch=1 enable_profiler=true \
         base_output_directory=gs://maxtext-logs-dogfood-proj/maxtext-logs/ \
         dataset_path=gs://maxtext-logs-dogfood-proj/dataset
